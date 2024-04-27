2fcc737cbc6ecb80ff59bbc56a89869aad62a460
NothileC <nothilecmhlanga@outlook.com>
2024-04-27 18:19:53 +0800

    updating hero page

---
+++ src/components/Shared/PrimaryButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ArrowRight } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+
+const PrimaryButton = () => {
+  return <>
+   <div className='flex items-center group'>
+   <Link to="/Recipelist"><button className='bg-primary h-[40px] text-white px-3 py-2 window.location.href='>See Our Dishes</button></Link>
+    <ArrowRight className='inline-block group-hover:translate-x-2 duration-200 p-2 text-base h-[40px] w-[40px] bg-primaryDark'/>
+
+  </div>
+  </>;
+  
+};
+
+export default PrimaryButton;
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import './Homepage.css';
 import Hero from '../../assets/Hero.jpg';
+import background from '../../assets/background.jpg';
+import PrimaryButton from '../../components/Shared/PrimaryButton';
+
+
+const Background = {
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  width: "100%",
+  height: "100%",
+  border: "10px",
+
+};
 
 const Homepage = () => {
   return <>
-    <div className='realtive z-[-1]'>
-      <div className="container">
+    <div style={Background}  className='realtive z-[-1]'>
+      <div className="container py-16 sm:py-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
           {/* text-content section */}
-          <div className="space-y-7 text-dark order-2 sm:order-1">
+          <div className="space-y-7 text-white order-2 sm:order-1">
             <h1 className="text-5xl">
-              Explore a World of Flavors, Aromas, and <span className='text-secondary font-cursive text-7xl'>Culinary</span>delights.
+              Explore a World of <span className=' font-cursive text-orange-700'>Flavors</span>, <span className='font-cursive text-red-700'>Aromas</span>, and <span className='text-secondary font-cursive text-7xl'>Culinary</span> delights.
 
             </h1>
-            <p>
+            <p className='lg:pr-64'>
               From mouth-watering mains to delectable desserts, embark on a culinary journey like never before!
             </p>
+
+            {/*button section*/}
+            <div>
+              <PrimaryButton ></PrimaryButton>
+            </div>
           </div>
           <div className='order-1 sm:order-2'>
             {/* Image section */}
-            <img src={Hero} alt="" />
+            <img src={Hero} alt="" className='w-full sm:scale-60 sm:-translate-y-16' />
           </div>
 
         </div>
