e96eb075570a0e758fc79fc8e74cbc3b821a5dc3
NothileC <nothilecmhlanga@outlook.com>
2024-05-01 00:49:27 +0800

    button added

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Homepage.css';
-import Hero from '../../assets/Hero.jpg';
-import background from '../../assets/background.jpg';
+import Hero from '../../assets/hero.png';
+import background from '../../assets/heroBg.png';
 import PrimaryButton from '../../components/Shared/PrimaryButton';
 
 
@@ -22,9 +22,9 @@ const Homepage = () => {
       <div className="container py-16 sm:py-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
           {/* text-content section */}
-          <div className="space-y-7 text-white order-2 sm:order-1">
+          <div className="space-y-7 text-dark order-2 sm:order-1">
             <h1 className="text-5xl">
-              Explore a World of <span className=' font-cursive text-orange-700'>Flavors</span>, <span className='font-cursive text-red-700'>Aromas</span>, and <span className='text-secondary font-cursive text-7xl'>Culinary</span> delights.
+             Fresh & Healthy <br></br>recipe dishes made out of love!
 
             </h1>
             <p className='lg:pr-64'>
